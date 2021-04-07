9308af9abec9ff9df7074042f9bd8bacf6e3f257
mrcs22 <marcusmoraes2010@hotmail.com>
2021-04-07 01:54:52 -0300

    Finalizando distribuição de cartas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+const container = document.querySelector(".container");
+//prettier-ignore
+const imageNames = ["bobross","explody","fiesta","metal","revertit","triplets","unicorn"];
 let receivedNumber = null;
 
 function start() {
   askNumberOfCards();
+  renderCards(receivedNumber);
 }
+
+start();
 
 function askNumberOfCards() {
   //prettier-ignore
@@ -11,4 +17,42 @@ function askNumberOfCards() {
     }
 }
 
-start();
+function renderCards() {
+  const imageNames = getRandomImageNames();
+
+  imageNames.forEach((imageName) => renderCardDiv(imageName));
+}
+
+function getRandomImageNames() {
+  let randomImageNames = [];
+
+  while (randomImageNames.length < receivedNumber / 2) {
+    const randomIndex = parseInt(Math.random() * 7);
+    const name = imageNames[randomIndex];
+
+    if (randomImageNames.indexOf(name) === -1) {
+      randomImageNames.push(name);
+    }
+  }
+
+  randomImageNames = [...randomImageNames, ...randomImageNames];
+
+  return randomImageNames;
+}
+
+function renderCardDiv(name) {
+  const div = document.createElement("div");
+  div.classList.add("card");
+
+  const backImage = document.createElement("img");
+  backImage.classList.add("back-card");
+  backImage.setAttribute("src", `./img/${name}parrot.gif`);
+
+  const frontImage = document.createElement("img");
+  frontImage.setAttribute("src", "./img/front.jpeg");
+
+  div.appendChild(backImage);
+  div.appendChild(frontImage);
+
+  container.appendChild(div);
+}
